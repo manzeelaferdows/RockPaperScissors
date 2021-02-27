f2899dd7593867bc75d6a8d9e425325d36dc6cf5
amberleeshand <amberleeshand@gmail.com>
2021-02-27 21:32:45 +0000

    Added rules of the game

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -1,16 +1,29 @@
 import random
 #get user input
+def get_rules_of_rock_paper_scissors():
+    print("Welcome to Rock, Paper, Scissors!")
+    instructions = (input("Do you know the rules of the game? Type 'y' for yes and 'n' for no ")).lower()
+    if (instructions == 'y') or (instructions == 'yes'):
+        return "Let's play!"
+    elif (instructions == 'n') or (instructions == 'no'):
+        return "To play the game, you have the option to pick either rock, paper or scissors\nTo win, rock beats scissors, scissors beats paper and paper beats rock\nYour opponent is the computer, may the best player win!"
+    else:
+        return "Input not recognised. Please type 'y' for yes and 'n' for no."
 
 def get_user_choice():
     global user_choice
     user_choice = (input("Do you want 'r' Rock, 'p' Paper or 's' Scissors? ")).lower()
     if (user_choice == 'r') or (user_choice == 'rock'):
+        user_choice = "rock"
         return f"User choice is: {user_choice}"
     elif (user_choice == 'p') or (user_choice == 'paper'):
+        user_choice = "paper"
         return f"User choice is: {user_choice}"
     elif (user_choice == 's') or (user_choice == 'scissors'):
+        user_choice = "scissors"
         return f"User choice is: {user_choice}"
     else:
+        user_choice = "invalid"
         return "Error! Not a valid choice"
 #def user_choice(choice):
 def get_computer_choice():
@@ -27,13 +40,17 @@ def get_computer_choice():
         return "Computer choice is: paper"
 
 def determine_winner(user_choice, computer_choice):
-    if user_choice == computer_choice:
+    if user_choice == "invalid":
+        return "Your choice is invalid! Try again."
+    elif user_choice == computer_choice:
         return f"It's a tie. You both picked {computer_choice}"
     elif (user_choice == "rock" and computer_choice == "scissors") or (user_choice == "paper" and computer_choice == "rock") or (user_choice == "scissors" and computer_choice == "paper"):
         return f"User wins! {user_choice.capitalize()} wins against {computer_choice}"
     else:
         return f"Computer wins! {computer_choice.capitalize()} wins against {user_choice}"
 
+
+print(get_rules_of_rock_paper_scissors())
 print(get_user_choice())
 print(get_computer_choice())
 print(determine_winner(user_choice, computer_choice))
