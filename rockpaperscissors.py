07d6a81c7af6bae2579cc6cf7dfea57e623ed16b
amberleeshand <amberleeshand@gmail.com>
2021-02-27 15:57:00 +0000

    Made final changes

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -1,39 +1,42 @@
 import random
 #get user input
 
-def play():
+def get_user_choice():
+    global user_choice
     user_choice = (input("Do you want 'r' Rock, 'p' Paper or 's' Scissors? ")).lower()
     if (user_choice == 'r') or (user_choice == 'rock'):
-        return(f"User choice is: {user_choice}")
+        return f"User choice is: {user_choice}"
     elif (user_choice == 'p') or (user_choice == 'paper'):
-        return(f"User choice is: {user_choice}")
+        return f"User choice is: {user_choice}"
     elif (user_choice == 's') or (user_choice == 'scissors'):
-        return(f"User choice is: {user_choice}")
+        return f"User choice is: {user_choice}"
     else:
-        return("Error! Not a valid choice")
+        return "Error! Not a valid choice"
 #def user_choice(choice):
-def get_computer_choice(computer_choice):
+def get_computer_choice():
+    global computer_choice
     computer_choice = random.randint(0, 2)
     if computer_choice == 0:
         computer_choice = "rock"
-        print(computer_choice)
+        return "Computer choice is: rock"
     elif computer_choice == 1:
         computer_choice = "scissors"
-        print(computer_choice)
+        return "Computer choice is: scissors"
     else:
         computer_choice = "paper"
-        print(computer_choice)
+        return "Computer choice is: paper"
 
 def determine_winner(user_choice, computer_choice):
     if user_choice == computer_choice:
-        print(f"It's a tie. You both picked {computer_choice}")
+        return (f"It's a tie. You both picked {computer_choice}")
     elif (user_choice == "rock" and computer_choice == "scissors") or (user_choice == "paper" and computer_choice == "rock") or (user_choice == "scissors" and computer_choice == "paper"):
-        print("user choice wins")
+        return (f"User wins! The user choice was {user_choice} and computer choice was {computer_choice}")
     else:
-        print("computer wins")
+        return (f"Computer wins! The computer choice was {computer_choice} and user choice was {user_choice}")
 
-determine_winner("rock", "paper")
-
+print(get_user_choice())
+print(get_computer_choice())
+print(determine_winner(user_choice, computer_choice))
 
 
 #use random to calculate the computer choice
